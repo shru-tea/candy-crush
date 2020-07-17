889d23794aa2e73c3e600e1bbadb762f99e15b6a
Shruti <shruti2000srivastava@gmail.com>
2020-07-17 14:50:42 +0530

    JavaScript update v1

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
-  const width = 8;
+  const width = 8; //number of columns and rows
+  const squares = []; //every div that is created
+
+  const candyColors = ["red", "yellow", "orange", "purple", "green", "blue"];
+  //Create Board
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement("div");
+      //giving a random color to the div being created
+      let randomColor = Math.floor(Math.random() * candyColors.length);
+      square.style.backgroundColor = candyColors[randomColor];
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
 });
